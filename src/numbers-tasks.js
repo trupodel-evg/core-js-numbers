0954aeee2ce6a89f83f497777a03e1eec002268a
Evgenii Sudilovskii <vaipipipi@mail.ru>
2025-10-15 15:27:42 +0300

    refactor: task 3

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -50,10 +50,7 @@ function getCircleCircumference(radius) {
  *  -3, 3  => 0
  */
 function getAverage(value1, value2) {
-  if (value1 > Number.MAX_VALUE - value2) {
-    return Number.MAX_VALUE;
-  }
-  return (value1 + value2) / 2;
+  return value1 / 2 + value2 / 2;
 }
 
 /**
